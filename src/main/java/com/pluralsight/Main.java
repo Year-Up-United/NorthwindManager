145e0122d9aaf52fbcc0f415638a0b40170a81dd
thejaylajones <janeissaj@gmail.com>
2025-06-11 10:12:25 -0400

    wed class

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import com.pluralsight.UserInterface.UI;
+
+public class Main {
+    public static void main(String[] args) {
+        UI ui = new UI();
+        ui.display();
+
+        }
+    }
---
+++ src/main/java/com/pluralsight/UserInterface/UI.java
@@ -0,0 +1,13 @@
+package com.pluralsight.UserInterface;
+
+public class UI {
+
+    public void display() {
+        System.out.println("Welcome to the Northwind Database");
+
+    }
+
+    private Employee loginUser(){
+
+    }
+}
